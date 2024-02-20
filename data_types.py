1a766c3561e1da7adc93c816df967db26554ec25
edudin <edudin007@gmail.com>
2024-02-20 20:35:40 -0300

    versão inicial

---
+++ data_types.py
@@ -0,0 +1,17 @@
+#string data type
+
+#literal assignment
+
+first = 'Edu'
+last = 'Moraes'
+
+#print(type(first))
+#print(type(first) == str)
+#print(isinstance(first, str))
+
+#constructor function   
+
+pizza = str("Pepperoni")
+#print(type(pizza))
+#print(type(pizza) == str)
+#print(isinstance(pizza, str))
